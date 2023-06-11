0014c89b7d855a2dd1960efe77a2f484c8308f82
Yushan Fernando <yushanwebdev@gmail.com>
2023-06-11 08:00:24 +0000

    feat: List Page Development - Develop the basic version of the `<BackArrowBtn />` Component

---
+++ app/list/back-arrow-btn.tsx
@@ -0,0 +1,9 @@
+import IconBackArrow from "./icon-back-arrow";
+
+export default function BackArrowBtn() {
+  return (
+    <button className="p-2 bg-white rounded-full drop-shadow-one">
+      <IconBackArrow />
+    </button>
+  );
+}
